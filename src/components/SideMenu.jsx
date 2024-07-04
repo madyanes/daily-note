1a3c080e1d339425f958d803757e0646623bf254
madyanes <madyanes.dev@gmail.com>
2024-07-04 21:28:59 +0700

    Add new note menu

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,7 +1,16 @@
 export default function SideMenu() {
   return (
     <section id='side-menu'>
-      <a href='#'>My Notes</a>
+      <nav>
+        <ul>
+          <li>
+            <a href='#'>My Notes</a>
+          </li>
+          <li id='new-note'>
+            <a href='#'>New Note</a>
+          </li>
+        </ul>
+      </nav>
     </section>
   )
 }
