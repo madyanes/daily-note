8fe19a71793a4e59fcfd53899125fd97a735620a
madyanes <madyanes.dev@gmail.com>
2024-07-06 01:40:15 +0700

    Create store to save note
    By default, the store name in indexedDB is localforage which doesn't make
    sense for this kind of data. So I create my own note store.

--- src/components/QuillEditor.jsx
+++ src/components/QuillEditor.jsx
@@ -1,15 +1,31 @@
 import { useEffect, useRef } from 'react'
 import Quill from 'quill'
+import localforage from 'localforage'
+
 import 'quill/dist/quill.snow.css'
+
+import { store } from '../utils/dbConfig'
 
 export default function QuillEditor() {
   const quillRef = useRef(null)
   const quillInstance = useRef(null)
+  const noteStoreRef = useRef(null)
 
   useEffect(() => {
-    if (!quillInstance.current && quillRef.current) {
-      quillInstance.current = new Quill(quillRef.current, { theme: 'snow' })
+    const initializeQuill = () => {
+      if (!quillInstance.current && quillRef.current) {
+        quillInstance.current = new Quill(quillRef.current, { theme: 'snow' })
+      }
     }
+
+    const initializeStores = () => {
+      if (!noteStoreRef.current) {
+        noteStoreRef.current = localforage.createInstance(store.notes)
+      }
+    }
+
+    initializeQuill()
+    initializeStores()
   }, [])
 
   return (
---
+++ src/utils/dbConfig.js
@@ -0,0 +1,9 @@
+const dbName = 'daily-note'
+
+export const store = {
+  notes: {
+    name: dbName,
+    storeName: 'notes',
+    description: 'This store saves daily notes',
+  },
+}
