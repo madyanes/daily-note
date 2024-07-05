1839873bc5331979e397428827f5206e385b227c
madyanes <madyanes.dev@gmail.com>
2024-07-05 07:54:45 +0700

    Create save note button

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,9 +1,12 @@
 import QuillEditor from './QuillEditor'
 
+import '../styles/NewNote.css'
+
 export default function NewNote() {
   return (
     <section id='new-note'>
       <QuillEditor />
+      <button id='save-note'>Save</button>
     </section>
   )
 }
