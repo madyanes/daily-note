e5a1077a40b73c9a81618d9a25c3859cc73ca436
madyanes <madyanes.dev@gmail.com>
2024-07-08 10:35:48 +0700

    Create note action buttons
    - Archive button
    - Update button
    - Delete button

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,27 @@
+import { TfiTrash, TfiPencilAlt, TfiArchive } from 'react-icons/tfi'
+
+import '../styles/Note.css'
+
 export default function Note({ note }) {
   return (
     <>
-      <article
-        dangerouslySetInnerHTML={{ __html: note.note }}
-        className='notes'
-      />
+      <div className='wrapper-note'>
+        <div className='controls-note'>
+          <button>
+            <TfiArchive className='control-archive-icon' />
+          </button>
+          <button>
+            <TfiPencilAlt className='control-update-icon' />
+          </button>
+          <button>
+            <TfiTrash className='control-delete-icon' />
+          </button>
+        </div>
+        <article
+          dangerouslySetInnerHTML={{ __html: note.note }}
+          className='notes'
+        />
+      </div>
     </>
   )
 }
