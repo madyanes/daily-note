c043519f87c91daba074ffeeb4f24e8804d8a868
madyanes <madyanes.dev@gmail.com>
2024-07-15 21:58:22 +0700

    Fix: remove state because already using loader data

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -2,11 +2,9 @@ import { useLoaderData } from 'react-router-dom'
 import QuillEditor from './QuillEditor'
 
 import '../styles/NewNote.css'
-import { useState } from 'react'
 
 export default function NewNote() {
-  const loaderData = useLoaderData() || ''
-  const [note] = useState(loaderData)
+  const note = useLoaderData() || ''
 
   return (
     <section id='new-note'>
