0a4d98b508e0250057f605afcf6edd267454f91a
madyanes <madyanes.dev@gmail.com>
2024-07-15 22:05:53 +0700

    Fix: edited note content is loaded in new note
    Saat mengedit catatan, lalu beralih ke pembuatan catatan baru, isi dari
    catatan sebelumnya termuat di catatan baru (bug).

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,13 +1,17 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useParams } from 'react-router-dom'
 import QuillEditor from './QuillEditor'
 
 import '../styles/NewNote.css'
 
 export default function NewNote() {
   const note = useLoaderData() || ''
+  const { noteId } = useParams()
 
   return (
-    <section id='new-note'>
+    // Ketika membuka Quill editor untuk edit catatan, kemudian langsung membuka Quill editor untuk membuat catatan baru,
+    // isi dari catatan sebelumnya termuat di Quill editor untuk catatan baru.
+    // Masalah tsb bisa diatasi dengan menambahkan properti `key`.
+    <section id='new-note' key={noteId || ''}>
       <QuillEditor existingNote={note || {}} />
     </section>
   )
