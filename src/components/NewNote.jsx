bed72216f22006efcd0566686ed3ef674e6765eb
madyanes <madyanes.dev@gmail.com>
2024-07-15 11:05:22 +0700

    Fix: error fetching data from loader

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -2,9 +2,17 @@ import { useLoaderData } from 'react-router-dom'
 import QuillEditor from './QuillEditor'
 
 import '../styles/NewNote.css'
+import { useEffect, useState } from 'react'
 
 export default function NewNote() {
-  const { note } = useLoaderData()
+  const loaderData = useLoaderData()
+  const [note, setNote] = useState('')
+
+  useEffect(() => {
+    if (loaderData) {
+      setNote(loaderData.note)
+    }
+  }, [loaderData])
 
   return (
     <section id='new-note'>
